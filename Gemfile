153d4c435e4c2ea37d2a768e778b1b36e4b4f718
fumtas1k <fumta01a7@gmail.com>
2022-04-03 18:38:07 +0900

    add:actiontextインストールしuserにavatar追加

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem 'image_processing'
 gem 'bootsnap', '>= 1.4.2', require: false
+
+gem 'actiontext'
 gem 'devise'
 gem 'devise-i18n'
 gem 'faker'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable
+
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [200, 200]
+  end
+
+  validates :name, presence: true, length: {maximum: 255}
+  validates :code, presence: true, length: {maximum: 6}
+  validates :mentor, presence: true
+  attribute :mentor, :string, default: false
+  validates :entered_at, presence: true
+  validates :admin, presence: true
+  attribute :admin, :boolean, default: false
 end
