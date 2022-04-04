1d346d0d8a39cbcf025206cf4c5613e33f46dba5
fumtas1k <fumta01a7@gmail.com>
2022-04-04 18:08:05 +0900

    add:userモデルのバリデーションテスト追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,34 @@
 FactoryBot.define do
   factory :user do
-    
+    name      { "firstuser" }
+    code      { "000001" }
+    email     { "first@diver.com" }
+    entered_at{ Date.new(Date.current.year, 4, 1) }
+    password  { "password" }
+
+    trait :seq do
+      sequence :name, "user01"
+      sequence :code, "100001"
+      sequence :email, "user01@diver.com"
+    end
+
   end
+
+  factory :mentor, class: User do
+    name    { "mentor" }
+    code    { "200001" }
+    email   { "mentor@diver.com" }
+    entered_at { Date.new(2020, 4, 1) }
+    mentor  { true }
+  end
+
+  factory :admin, class: User do
+    name    { "admin" }
+    code    { "300001" }
+    email   { "admin@diver.com" }
+    entered_at { Date.new(2014, 4, 1) }
+    mentor  { true }
+    admin   { true }
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーションのテスト" do
+    let(:user) { build(:user) }
+    shared_examples "バリデーションに引っかかる" do
+      it {expect(user).to_not be_valid}
+    end
+
+    context "必須事項を入力した場合" do
+      it "バリデーションが通る" do
+        expect(user).to be_valid
+      end
+    end
+
+    context "名前が空白の場合" do
+      before {user.name = ""}
+      it_behaves_like "バリデーションに引っかかる"
+    end
+
+    context "codeが空白の場合" do
+      before {user.code = ""}
+      it_behaves_like "バリデーションに引っかかる"
+    end
+
+    context "emailが空白の場合" do
+      before {user.email = ""}
+      it_behaves_like "バリデーションに引っかかる"
+    end
+
+    context "entered_atが空白の場合" do
+      before {user.entered_at = ""}
+      it_behaves_like "バリデーションに引っかかる"
+    end
+
+    context "passwordが空白の場合" do
+      before {user.password = ""}
+      it_behaves_like "バリデーションに引っかかる"
+    end
+
+    context "passwordが5文字の場合" do
+      before {user.password = "passw"}
+      it_behaves_like "バリデーションに引っかかる"
+    end
+  end
 end
