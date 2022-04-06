581b3d44f51d5a711a372566210b8bb49ac163a1
fumtas1k <fumta01a7@gmail.com>
2022-04-06 12:10:37 +0900

    add:grouping機能にajax追加

--- app/controllers/groupings_controller.rb
+++ app/controllers/groupings_controller.rb
@@ -1,11 +1,20 @@
 class GroupingsController < ApplicationController
   def create
-    current_user.group.groupings.create!(user_id: params[:member_id])
-    redirect_to users_path
+    @user = User.find(params[:member_id])
+    respond_to do |format|
+      current_user.group.groupings.create!(user_id: @user.id)
+      format.html {redirect_to users_path}
+      format.js {render "member_change"}
+    end
   end
 
   def destroy
-    Grouping.find(params[:id]).destroy
-    redirect_to users_path
+    grouping = Grouping.find(params[:id])
+    @user = grouping.user
+    respond_to do |format|
+      grouping.destroy
+      format.html {redirect_to users_path}
+      format.js {render "member_change"}
+    end
   end
 end
