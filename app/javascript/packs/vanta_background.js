085c34127ef9f12b9515087b71c9027e3e472c51
fumtas1k <fumta01a7@gmail.com>
2022-04-16 19:13:23 +0900

    add:ユーザー認証ページに背景としてvantaの動的背景を導入

---
+++ app/javascript/packs/vanta_background.js
@@ -0,0 +1,22 @@
+// VANTA.CLOUDS({
+//   el: "#vanta-background",
+//   mouseControls: true,
+//   touchControls: true,
+//   gyroControls: false,
+//   minHeight: 200.00,
+//   minWidth: 200.00
+// })
+
+VANTA.FOG({
+  el: "#vanta-background",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  highlightColor: 0xfff4df,
+  midtoneColor: 0xffeae6,
+  lowlightColor: 0xd3cbff,
+  baseColor: 0xfffafa,
+  speed: 2.00
+})
