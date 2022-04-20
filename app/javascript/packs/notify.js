3f3e75d7280262478aa8f5e4b0c23b569f47f1b4
fumtas1k <fumta01a7@gmail.com>
2022-04-20 14:06:04 +0900

    fix:構文修正202204201406

--- app/javascript/packs/notify.js
+++ app/javascript/packs/notify.js
@@ -1,5 +1,5 @@
 // ヒストリーバック時にリロードする設定(新着をクリックして戻った時用)
 // おそらく推奨されないため、場合によっては削除する
-window.addEventListener('pageshow',()=>{
-  if(window.performance.navigation.type==2) location.reload();
+window.addEventListener('pageshow',() => {
+  if(window.performance.navigation.type === 2) location.reload();
 });
