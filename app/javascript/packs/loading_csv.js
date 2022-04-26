82fe89a89402196da74ab1e9170b4ff5c9d6dead
fumtas1k <fumta01a7@gmail.com>
2022-04-26 14:35:14 +0900

    improve:jqueryが反応しない場合があったため記載場所変更2

--- app/javascript/packs/loading_csv.js
+++
@@ -1,6 +0,0 @@
-$(document).on('turbolinks:load', function() {
-  $("#import-btn").on("click", function(){
-    $("#load-image-container").removeClass("none-display");
-    $(".alert").addClass("none-display");
-  });
-});
