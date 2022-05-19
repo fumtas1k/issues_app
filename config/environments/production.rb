542da828c1ffa5dc109b1391b917f3ebace2c306
fumtas1k <fumta01a7@gmail.com>
2022-05-19 11:36:44 +0900

    add:本番環境でactioncable使用できるよう設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://warm-scrubland-25965.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://warm-scrubland-25965.herokuapp.com/', 'http://warm-scrubland-25965.herokuapp.com/']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
