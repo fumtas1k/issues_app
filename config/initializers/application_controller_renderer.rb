2e902f9b5f9d8817517af6cbfa6ddc4152b36c12
fumtas1k <fumta01a7@gmail.com>
2022-05-19 21:17:32 +0900

    fix:構文修正202205192117

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,13 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
 ActiveSupport::Reloader.to_prepare do
-  ApplicationController.renderer.defaults.merge!(
-    if Rails.env.production?
-      http_host: "warm-scrubland-25965.herokuapp.com"
+  if Rails.env.production?
+    ApplicationController.renderer.defaults.merge!(
+      http_host: "warm-scrubland-25965.herokuapp.com",
       https: true
-    else
-      http_host: "localhost:3000"
+    )
+  else
+    ApplicationController.renderer.defaults.merge!(
+      http_host: "localhost:3000",
       https: false
-    end
-  )
+    )
+  end
 end
