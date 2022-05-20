ef20a92006011fb705dc1a3d2b17008e0146d7b5
fumtas1k <fumta01a7@gmail.com>
2022-05-20 22:13:25 +0900

    add:chat_roomsにmessages_count追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_18_141120) do
+ActiveRecord::Schema.define(version: 2022_05_20_130334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2022_05_18_141120) do
   create_table "chat_rooms", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "messages_count", default: 0, null: false
   end
 
   create_table "comments", force: :cascade do |t|
