e662806f51624dfdcc031b8d270fe190d632d08f
fumtas1k <fumta01a7@gmail.com>
2023-01-03 15:15:12 +0900

    [change]更新時間変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,12 +1,13 @@
 rails_env = ENV["RAILS_ENV"] || "production"
 set :output, 'log/crontab.log'
 set :environment, rails_env
-ENV.each {|k, v| env(k, v)}
 
-every "10 0 * * *" do
+ENV.each {|k, v| env(k, v)} if rails_env == "development"
+
+every "0 0 * * *" do
   rake "heroku_scheduler:delete_notification"
 end
 
-every "10 1 * * *" do
+every "0 1 * * *" do
   rake "heroku_scheduler:purge_unattached"
 end
