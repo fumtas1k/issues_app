84362ed9e3c8a4a4d78b7d6d5550401e5d86c07a
fumtas1k <fumta01a7@gmail.com>
2023-01-03 16:12:44 +0900

    [change]時間と分岐変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,14 +1,14 @@
 rails_env = ENV["RAILS_ENV"] || "production"
 set :output, 'log/crontab.log'
-set :environment, rails_env if rails_env == "development"
+set :environment, rails_env
 
-ENV.each {|k, v| env(k, v)}
+ENV.each {|k, v| env(k, v)} if rails_env == "development"
 
 # テスト用に時間を変更revertする
-every "10 16 * * *" do
+every "0 0 * * *" do
   rake "heroku_scheduler:delete_notification"
 end
 
-every "15 16 * * *" do
+every "20 16 * * *" do
   rake "heroku_scheduler:purge_unattached"
 end
