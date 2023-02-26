3249a2c099241b1e234132283aa7dd5857f5c1a9
fumtas1k <fumta01a7@gmail.com>
2023-02-26 20:06:43 +0900

    [change]サーバー、ユーザー名変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # server-based syntax
-server "18.177.55.146", user: "app", roles: %w{app db web}
+server "13.115.29.135", user: "ec2-user", roles: %w{app db web}
 
 # Global options
-set :ssh_options, keys: '/Users/fumta/.ssh/id_rsa'
+set :ssh_options, keys: [File.expand_path('~/.ssh/aws2023.pem')]
