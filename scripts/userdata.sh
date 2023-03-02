9e6093db290c958d14c1e39e6cf9160df1b231cb
fumtas1k <fumta01a7@gmail.com>
2023-03-02 20:56:17 +0900

    [add]ユーザーデータのスクリプト追加

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+sudo yum update -y
+sudo timedatectl set-timezone Asia/Tokyo
+sudo localectl set-locale LANG=ja_JP.utf8
+sudo source /etc/locale.conf
+sudo amazon-linux-extras install -y nginx1 postgresql12
+sudo yum install -y gcc-c++ make patch git openssl-devel sudo readline-devel zlib-devel ImageMagick-devel curl libffi-devel libicu-devel libxml2-devel libxslt-devel postgresql-server postgresql-devel poppler-utils poppler-data
+sudo systemctl restart crond.service
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo /usr/bin/postgresql-setup --initdb --unit postgresql
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
 sudo mkdir /usr/local/rbenv
 sudo chown $USER /usr/local/rbenv
 git clone https://github.com/rbenv/rbenv.git /usr/local/rbenv
@@ -17,4 +29,5 @@ npm install --location=global yarn
 sudo mkdir /var/www
 sudo chown $USER /var/www
 mkdir -p /var/www/issues_app/shared
+touch /var/www/issues_app/shared/.env
 gem install bundler
